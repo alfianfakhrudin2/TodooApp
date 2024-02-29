d5699be93f997e78b11e2c833cf8c177320141c6
Alfian <alfianfakhrudin11@gmail.com>
2024-03-01 04:43:39 +0700

    menambahkan intent dari welcome ke onboarding screen

--- app/src/main/java/com/example/mytask/WelcomeActivity.kt
+++ app/src/main/java/com/example/mytask/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package com.example.mytask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
         supportActionBar?.hide()
+
+        val button  = findViewById<Button>(R.id.btn_started)
+        button.setOnClickListener {
+            val intent = Intent(this, OnBoardingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
