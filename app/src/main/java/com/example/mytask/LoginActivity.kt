ba48efa5a33bd098d5351b4b621aa9dea912658e
Alfian <alfianfakhrudin11@gmail.com>
2024-03-02 02:21:36 +0700

    menambahkan intent pada btn logout

--- app/src/main/java/com/example/mytask/LoginActivity.kt
+++ app/src/main/java/com/example/mytask/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mytask.databinding.ActivityLoginBinding
 
+
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/mytask/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mytask/ui/profile/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package com.example.mytask.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.mytask.LoginActivity
 import com.example.mytask.R
 
 
@@ -15,5 +18,22 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Implementasi ketika tombol di klik
+        view.findViewById<Button>(R.id.btn_logout).setOnClickListener {
+            // Membuat Intent untuk berpindah ke Activity
+            val intent = Intent(activity, LoginActivity::class.java)
+
+            // (Opsional) Mengirim data menggunakan Intent
+            intent.putExtra("key", "value")
+
+            // Memulai Activity baru
+            startActivity(intent)
+        }
+
     }
 }
