aedc868eae56d03f79ff391df7575f070bea7388
Alfian <alfianfakhrudin11@gmail.com>
2024-03-03 19:55:41 +0700

    update fragment binding

--- app/src/main/java/com/example/mytask/ui/task/TaskFragment.kt
+++ app/src/main/java/com/example/mytask/ui/task/TaskFragment.kt
@@ -1,22 +1,24 @@
 package com.example.mytask.ui.task
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
+import com.example.mytask.R
 import com.example.mytask.databinding.FragmentTaskBinding
+
+import com.example.mytask.ui.addTask.AddTaskFragment
 
 class TaskFragment : Fragment() {
 
-    private var _binding: FragmentTaskBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentTaskBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,22 +28,36 @@ class TaskFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(TaskViewModel::class.java)
 
-        _binding = FragmentTaskBinding.inflate(inflater, container, false)
+        binding = FragmentTaskBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textTask
+        val textView = binding.textTask
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.btnTask.setOnClickListener {
+            // Panggil fungsi untuk berpindah ke AddTaskFragment
+            navigateToAddTaskFragment()
+        }
+
         return root
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        (activity as AppCompatActivity?)?.supportActionBar?.hide()
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun navigateToAddTaskFragment() {
+        // Buat instance dari AddTaskFragment
+        val addTaskFragment = AddTaskFragment()
+
+        // Mulai transaksi fragment
+        val transaction: FragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+
+        // Ganti containerFragment dengan id dari container tempat AddTaskFragment akan ditampilkan
+        transaction.replace(R.id.container, addTaskFragment)
+
+        // (Opsional) Tambahkan ke back stack, sehingga dapat kembali ke TaskFragment jika diperlukan
+        transaction.addToBackStack(null)
+
+        // Jalankan transaksi
+        transaction.commit()
     }
 }
