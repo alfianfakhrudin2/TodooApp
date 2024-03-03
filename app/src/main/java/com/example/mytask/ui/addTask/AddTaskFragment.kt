e22af083840a7aaadf9be516302881c851c6ba58
Alfian <alfianfakhrudin11@gmail.com>
2024-03-03 16:05:43 +0700

    menambahkan add task screen

---
+++ app/src/main/java/com/example/mytask/ui/addTask/AddTaskFragment.kt
@@ -0,0 +1,31 @@
+package com.example.mytask.ui.addTask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mytask.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AddTaskFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AddTaskFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_task, container, false)
+    }
+
+}
